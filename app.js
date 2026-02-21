ed9899b8bdb337091474eb823e205636c8dea774
perrinbm97 <perrinbm08@gmail.com>
2026-02-21 17:04:04 -0500

    First

---
+++ app.js
@@ -0,0 +1,19 @@
+/**Print every character from a string on individual rows
+ * "Endurance"
+ * 
+ * E
+ * n
+ * d
+ * u
+ * r
+ * a
+ * n
+ * c
+ * e
+ */
+
+const str = 'Frontend Simplified'
+
+for (i=0; i<str.length; i++){
+    console.log(str[i]);
+}
